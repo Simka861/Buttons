cf07f9a060b84201c828cb47fbcaf1f6d3a54a57
user <user@WIN-O9NLDFUM70V>
2022-10-12 20:07:40 +0300

    Добавьте файлы проекта.

---
+++ Buttons/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Buttons
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Blue = new System.Windows.Forms.Button();
+            this.Aqua = new System.Windows.Forms.Button();
+            this.Teal = new System.Windows.Forms.Button();
+            this.Lime = new System.Windows.Forms.Button();
+            this.Yellow = new System.Windows.Forms.Button();
+            this.Green = new System.Windows.Forms.Button();
+            this.Olive = new System.Windows.Forms.Button();
+            this.Maroon = new System.Windows.Forms.Button();
+            this.Fuchsia = new System.Windows.Forms.Button();
+            this.Red = new System.Windows.Forms.Button();
+            this.Orange = new System.Windows.Forms.Button();
+            this.Silver = new System.Windows.Forms.Button();
+            this.Gray = new System.Windows.Forms.Button();
+            this.Black = new System.Windows.Forms.Button();
+            this.Purple = new System.Windows.Forms.Button();
+            this.White = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Navy";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Blue
+            // 
+            this.Blue.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Blue.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Blue.Location = new System.Drawing.Point(3, 32);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(43, 23);
+            this.Blue.TabIndex = 1;
+            this.Blue.Text = "Blue";
+            this.Blue.UseVisualStyleBackColor = false;
+            this.Blue.Click += new System.EventHandler(this.Blue_Click);
+            // 
+            // Aqua
+            // 
+            this.Aqua.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Aqua.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Aqua.Location = new System.Drawing.Point(3, 61);
+            this.Aqua.Name = "Aqua";
+            this.Aqua.Size = new System.Drawing.Size(43, 23);
+            this.Aqua.TabIndex = 3;
+            this.Aqua.Text = "Aqua";
+            this.Aqua.UseVisualStyleBackColor = false;
+            this.Aqua.Click += new System.EventHandler(this.Aqua_Click);
+            // 
+            // Teal
+            // 
+            this.Teal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Teal.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Teal.Location = new System.Drawing.Point(3, 90);
+            this.Teal.Name = "Teal";
+            this.Teal.Size = new System.Drawing.Size(43, 23);
+            this.Teal.TabIndex = 2;
+            this.Teal.Text = "Teal";
+            this.Teal.UseVisualStyleBackColor = false;
+            this.Teal.Click += new System.EventHandler(this.Teal_Click);
+            // 
+            // Lime
+            // 
+            this.Lime.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Lime.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Lime.Location = new System.Drawing.Point(107, 61);
+            this.Lime.Name = "Lime";
+            this.Lime.Size = new System.Drawing.Size(43, 23);
+            this.Lime.TabIndex = 7;
+            this.Lime.Text = "Lime";
+            this.Lime.UseVisualStyleBackColor = false;
+            this.Lime.Click += new System.EventHandler(this.Lime_Click);
+            // 
+            // Yellow
+            // 
+            this.Yellow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Yellow.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Yellow.Location = new System.Drawing.Point(107, 90);
+            this.Yellow.Name = "Yellow";
+            this.Yellow.Size = new System.Drawing.Size(55, 23);
+            this.Yellow.TabIndex = 6;
+            this.Yellow.Text = "Yellow";
+            this.Yellow.UseVisualStyleBackColor = false;
+            this.Yellow.Click += new System.EventHandler(this.Yellow_Click);
+            // 
+            // Green
+            // 
+            this.Green.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Green.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Green.Location = new System.Drawing.Point(107, 32);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(48, 23);
+            this.Green.TabIndex = 5;
+            this.Green.Text = "Green";
+            this.Green.UseVisualStyleBackColor = false;
+            this.Green.Click += new System.EventHandler(this.Green_Click);
+            // 
+            // Olive
+            // 
+            this.Olive.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Olive.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Olive.Location = new System.Drawing.Point(107, 3);
+            this.Olive.Name = "Olive";
+            this.Olive.Size = new System.Drawing.Size(43, 23);
+            this.Olive.TabIndex = 4;
+            this.Olive.Text = "Olive";
+            this.Olive.UseVisualStyleBackColor = false;
+            this.Olive.Click += new System.EventHandler(this.Olive_Click);
+            // 
+            // Maroon
+            // 
+            this.Maroon.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Maroon.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Maroon.Location = new System.Drawing.Point(199, 61);
+            this.Maroon.Name = "Maroon";
+            this.Maroon.Size = new System.Drawing.Size(65, 23);
+            this.Maroon.TabIndex = 11;
+            this.Maroon.Text = "Maroon";
+            this.Maroon.UseVisualStyleBackColor = false;
+            this.Maroon.Click += new System.EventHandler(this.Maroon_Click);
+            // 
+            // Fuchsia
+            // 
+            this.Fuchsia.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Fuchsia.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Fuchsia.Location = new System.Drawing.Point(199, 90);
+            this.Fuchsia.Name = "Fuchsia";
+            this.Fuchsia.Size = new System.Drawing.Size(56, 23);
+            this.Fuchsia.TabIndex = 10;
+            this.Fuchsia.Text = "Fuchsia";
+            this.Fuchsia.UseVisualStyleBackColor = false;
+            this.Fuchsia.Click += new System.EventHandler(this.Fuchsia_Click);
+            // 
+            // Red
+            // 
+            this.Red.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Red.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Red.Location = new System.Drawing.Point(199, 32);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(35, 23);
+            this.Red.TabIndex = 9;
+            this.Red.Text = "Red";
+            this.Red.UseVisualStyleBackColor = false;
+            this.Red.Click += new System.EventHandler(this.Red_Click);
+            // 
+            // Orange
+            // 
+            this.Orange.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Orange.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Orange.Location = new System.Drawing.Point(199, 3);
+            this.Orange.Name = "Orange";
+            this.Orange.Size = new System.Drawing.Size(56, 23);
+            this.Orange.TabIndex = 8;
+            this.Orange.Text = "Orange";
+            this.Orange.UseVisualStyleBackColor = false;
+            this.Orange.Click += new System.EventHandler(this.Orange_Click);
+            // 
+            // Silver
+            // 
+            this.Silver.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Silver.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Silver.Location = new System.Drawing.Point(295, 61);
+            this.Silver.Name = "Silver";
+            this.Silver.Size = new System.Drawing.Size(49, 23);
+            this.Silver.TabIndex = 15;
+            this.Silver.Text = "Silver";
+            this.Silver.UseVisualStyleBackColor = false;
+            this.Silver.Click += new System.EventHandler(this.Silver_Click);
+            // 
+            // Gray
+            // 
+            this.Gray.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Gray.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Gray.Location = new System.Drawing.Point(295, 90);
+            this.Gray.Name = "Gray";
+            this.Gray.Size = new System.Drawing.Size(49, 23);
+            this.Gray.TabIndex = 14;
+            this.Gray.Text = "Gray";
+            this.Gray.UseVisualStyleBackColor = false;
+            this.Gray.Click += new System.EventHandler(this.Gray_Click);
+            // 
+            // Black
+            // 
+            this.Black.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Black.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Black.Location = new System.Drawing.Point(295, 32);
+            this.Black.Name = "Black";
+            this.Black.Size = new System.Drawing.Size(49, 23);
+            this.Black.TabIndex = 13;
+            this.Black.Text = "Black";
+            this.Black.UseVisualStyleBackColor = false;
+            this.Black.Click += new System.EventHandler(this.Black_Click);
+            // 
+            // Purple
+            // 
+            this.Purple.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Purple.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Purple.Location = new System.Drawing.Point(295, 3);
+            this.Purple.Name = "Purple";
+            this.Purple.Size = new System.Drawing.Size(49, 23);
+            this.Purple.TabIndex = 12;
+            this.Purple.Text = "Purple";
+            this.Purple.UseVisualStyleBackColor = false;
+            this.Purple.Click += new System.EventHandler(this.Purple_Click);
+            // 
+            // White
+            // 
+            this.White.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.White.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.White.Location = new System.Drawing.Point(0, 119);
+            this.White.Name = "White";
+            this.White.Size = new System.Drawing.Size(46, 23);
+            this.White.TabIndex = 19;
+            this.White.Text = "White";
+            this.White.UseVisualStyleBackColor = false;
+            this.White.Click += new System.EventHandler(this.White_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.White);
+            this.Controls.Add(this.Silver);
+            this.Controls.Add(this.Gray);
+            this.Controls.Add(this.Black);
+            this.Controls.Add(this.Purple);
+            this.Controls.Add(this.Maroon);
+            this.Controls.Add(this.Fuchsia);
+            this.Controls.Add(this.Red);
+            this.Controls.Add(this.Orange);
+            this.Controls.Add(this.Lime);
+            this.Controls.Add(this.Yellow);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Olive);
+            this.Controls.Add(this.Aqua);
+            this.Controls.Add(this.Teal);
+            this.Controls.Add(this.Blue);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MainWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button Blue;
+        private Button Aqua;
+        private Button Teal;
+        private Button Lime;
+        private Button Yellow;
+        private Button Green;
+        private Button Olive;
+        private Button Maroon;
+        private Button Fuchsia;
+        private Button Red;
+        private Button Orange;
+        private Button Silver;
+        private Button Gray;
+        private Button Black;
+        private Button Purple;
+        private Button White;
+    }
+}
---
+++ Buttons/Form1.cs
@@ -0,0 +1,113 @@
+namespace Buttons
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.ForeColor = System.Drawing.Color.Navy;
+            Blue.ForeColor = System.Drawing.Color.Blue;
+            Aqua.ForeColor = System.Drawing.Color.Aqua;
+            Teal.ForeColor = System.Drawing.Color.Teal;
+            Olive.ForeColor = System.Drawing.Color.Olive;
+            Green.ForeColor = System.Drawing.Color.Green;
+            Lime.ForeColor = System.Drawing.Color.Lime;
+            Yellow.ForeColor = System.Drawing.Color.Yellow;
+            Orange.ForeColor = System.Drawing.Color.Orange;
+            Red.ForeColor = System.Drawing.Color.Red;
+            Maroon.ForeColor = System.Drawing.Color.Maroon;
+            Fuchsia.ForeColor = System.Drawing.Color.Fuchsia;
+            Purple.ForeColor = System.Drawing.Color.Purple;
+            Black.ForeColor = System.Drawing.Color.Black;
+            Silver.ForeColor = System.Drawing.Color.Silver;
+            Gray.ForeColor = System.Drawing.Color.Gray;
+            White.ForeColor = System.Drawing.Color.White;
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Navy;
+        }
+
+        private void Blue_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Blue;
+        }
+
+        private void Aqua_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Aqua;
+        }
+
+        private void Teal_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Teal;
+        }
+
+        private void Olive_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Olive;
+        }
+
+        private void Green_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Green;
+        }
+
+        private void Lime_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Lime;
+        }
+
+        private void Yellow_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Yellow;
+        }
+
+        private void Orange_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Orange;
+        }
+
+        private void Red_Click(object sender, EventArgs e)
+        {       
+            this.BackColor = System.Drawing.Color.Red;
+        }
+
+        private void Maroon_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Maroon;
+        }
+
+        private void Fuchsia_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Fuchsia;
+        }
+
+        private void Purple_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Purple;
+        }
+
+        private void Black_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Black;
+        }
+
+        private void Silver_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Silver;
+        }
+
+        private void Gray_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Gray;
+        }
+
+        private void White_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.White;
+        }
+    }
+}
